deda5ff19d8ae32b4924e6c53cd802877026f559
Albert Veli <albert.veli@gmail.com>
2011-10-08 00:53:39 +0200

    Add alsa capture with default sound device.

--- record_object.sh
+++ record_object.sh
@@ -7,9 +7,17 @@
 #
 # Albert Veli 2011
 
-# TODO: Ask if user wants to recond sound.
-#       If so, add -f alsa -ac 2 -i hw:0,0 -acodec pcm_s16le
-#       to ffmpeg flags.
+
+# Alsa device. Change this to use another
+# sound card than the default 'hw:0,0'. You could even
+# change it to 'pulse' to use pulseaudio instead.
+alsadev='hw:0,0'
+
+# Video codec for encoding. You might change this
+# if your ffmpeg does not support libx264. But
+# this setting gives best quality.
+vcodec='-vcodec libx264 -vpre lossless_ultrafast'
+
 
 if ! which xwininfo >/dev/null; then
     echo "install xwininfo first"
@@ -28,10 +36,23 @@ x11grab=`ffmpeg -codecs 2>/dev/null | grep -i libx264`
 echo "Click on the window you want to record (or on the background to record everything)."
 echo "When finished, quit recording by pressing q or Ctrl-C"
 
+read -p "Record sound? "
+if test "$REPLY" == "y"; then
+    # Record sound using Alsa
+    # (check rec volume in alsamixer if you get no sound)
+    snd='-f alsa -ac 2 -i $alsadev -acodec pcm_s16le'
+else
+    # No sound
+    snd='-an'
+fi
+
+# Get upper left corner, width and height using xwininfo.
 txt=`xwininfo`
-
+if test "$txt" == ""; then
+    echo "Error getting window information."
+    exit 1
+fi
 corner=`echo $txt | sed 's/^.*Corners://' | awk '{print $1}' | sed 's/\(+[0-9]*\)+/\1,/'`
-
 width=`echo $txt | sed 's/^.*Width://' | awk '{print $1}'`
 height=`echo $txt | sed 's/^.*Height://' | awk '{print $1}'`
 
@@ -40,4 +61,5 @@ read -p "Window is ${width}x${height}, continue (y/n)? "
 
 
 # ffmpeg may or may not work with the x11grab option, it depends on the platform and compile time flags
-ffmpeg -f x11grab -r 25 -s ${width}x${height} -i :0.0${corner} -an -vcodec libx264 -vpre lossless_ultrafast -threads 0 output.mkv
+ffmpeg -f x11grab -r 25 -s ${width}x${height} -i :0.0${corner} $snd $vcodec -threads 0 output.mkv
+
